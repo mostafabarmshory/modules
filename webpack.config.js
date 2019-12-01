68a4efabf8a3a777a70a32f4609e1cb379c1a3ef
mostafabarmshory <mostafa.barmshory@dpq.co.ir>
2019-12-01 09:28:01 +0330

    There is a typo in the name of the package

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
 		entry: {
-			app: './src/index.js',
-			bascket: './src/bascket.js',
+			basket: './src/basket.js',
 			user: './src/user.js'
 		},
 		plugins: [new CleanWebpackPlugin()],
